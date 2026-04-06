4971e3b3b01909178e54d05f6cc2b3ea3b892863
simeong-dkpg <simeongodwin01@gmail.com>
2026-04-06 13:39:16 +0100

    feat: create TipOfTheDay component with initial imports

---
+++ frontend/src/components/TipOfTheDay.tsx
@@ -0,0 +1,4 @@
+import { mockTips } from "@/lib/mock-data";
+import { Link } from "react-router-dom";
+import { formatDistanceToNow } from "date-fns";
+import { Star, ArrowRight } from "lucide-react";
