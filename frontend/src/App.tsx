1a7d580b7cc3bda2d0d3a90552e4dd3cf6e4f997
simeong-dkpg <simeongodwin01@gmail.com>
2026-04-06 13:13:36 +0100

    add App.tsx to set up main application structure and routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { WalletProvider } from "@/contexts/WalletContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Index from "./pages/Index";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Leaderboard from "./pages/Leaderboard";
+import ProofReceipt from "./pages/ProofReceipt";
+import NotFound from "./pages/NotFound";
