cb99fdac7d38180687c20ad1ad8757711f3aeec2
simeong-dkpg <simeongodwin01@gmail.com>
2026-05-17 15:47:39 +0100

    refactor: add AlertDialogOverlay component to AlertDialog for enhanced functionality

--- frontend/src/components/ui/alert-dialog.tsx
+++ frontend/src/components/ui/alert-dialog.tsx
@@ -9,3 +9,8 @@ const AlertDialog = AlertDialogPrimitive.Root;
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
 const AlertDialogPortal = AlertDialogPrimitive.Portal;
+
+const AlertDialogOverlay = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
+  
