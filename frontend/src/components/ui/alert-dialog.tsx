ac594de67557bd686a6e69396ee28b4b93c3e386
simeong-dkpg <simeongodwin01@gmail.com>
2026-05-17 15:48:06 +0100

    refactor: implement AlertDialogOverlay component for improved overlay functionality

--- frontend/src/components/ui/alert-dialog.tsx
+++ frontend/src/components/ui/alert-dialog.tsx
@@ -13,4 +13,14 @@ const AlertDialogPortal = AlertDialogPrimitive.Portal;
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
+  >(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Overlay
+    className={cn(
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      className,
+    )}
+    {...props}
+    ref={ref}
+  />
+));
   
